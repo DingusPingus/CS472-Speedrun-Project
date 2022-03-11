3de0aa71eb83b9a905563afa9c5e3d3ad9711adb
PsytruK <66977522+PsytruK@users.noreply.github.com>
2022-03-11 13:47:48 -0600

    Immediately Thought of Fixes for previous issues
    Applied bandaid fix of "If score 999999 (Default float assign) or 0 (default float value), say Not attempted.

--- Assets/Scripts/ScoreUpdater.cs
+++ Assets/Scripts/ScoreUpdater.cs
@@ -10,17 +10,36 @@ public class ScoreUpdater : MonoBehaviour
     public TMPro.TMP_Text highscoreLevel1;
     public TMPro.TMP_Text highscoreLevel2;
 
-    //int score1 = 999999;
-    //int score2 = 999999;
-    int score1;
-    int score2;
+    float score1 = 999999;
+    float score2 = 999999;
+    //float score1;
+    //float score2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        highscoreLevel1.text = PlayerPrefs.GetFloat("level1").ToString();
-        highscoreLevel2.text = PlayerPrefs.GetFloat("level2").ToString();
+        if (score1 > PlayerPrefs.GetFloat("level1")){
+            score1 = PlayerPrefs.GetFloat("level1");
+            highscoreLevel1.text = score1.ToString() + "s";
+        }
+
+        if (score2 > PlayerPrefs.GetFloat("level2"))
+        {
+            score2 = PlayerPrefs.GetFloat("level2");
+            highscoreLevel2.text = score2.ToString() + "s";
+        }
+
+        //Bandaid fix for No Attemt = 0s
+        if (score1 == 0 || score1 == 999999)
+        {
+            highscoreLevel1.text = "Not Attempted";
+        }
+        if (score2 == 0 || score2 == 999999)
+        {
+            highscoreLevel2.text = "Not Attempted";
+        }
+
 
     }
 
@@ -30,20 +49,4 @@ public class ScoreUpdater : MonoBehaviour
         
     }
 
-    public void ScoreUpdate()
-    {
-    
-    //PseudoCode
-    /*
-    if (currentScore > PlayerPrefs.GetFloat("level1"))
-        {
-            currentScore = PlayerPrefs.GetFloat("level1");
-        }
-    */
-
-
-
-    //    = PlayerPrefs.GetFloat("level1");
-    //    = PlayerPrefs.GetFloat("level2");
-    }
 }
