bdbc32942da75285a7277f326703a22dbcafe988
PsytruK <66977522+PsytruK@users.noreply.github.com>
2022-03-11 12:11:56 -0600

    Removed File, template for score
    PlayerPrefs.GetFloat("level1");

---
+++ Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScoreUpdate()
+    {
+    //    = PlayerPrefs.GetFloat("level1");
+    //    = PlayerPrefs.GetFloat("level2");
+    }
+}
