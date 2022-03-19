77ca2db6c87f8e46949f83307444f7d1739f3498
DingusPingus <isaacnathanielatta@gmail.com>
2022-03-18 19:27:31 -0600

    made sensitivity save between reloads

--- Assets/Scripts/OptionsFunctions.cs
+++ Assets/Scripts/OptionsFunctions.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class OptionsFunctions : MonoBehaviour
 {
     //Declares value for Custom Sensitivity, to prevent everything from being stuck.
-    public static float CustomSens = 5f;
+    public static float DefaultCustomSens = 5f;
 
     //Declaring slider variable to associate in game
     public Slider sensitivitySlider;
@@ -15,7 +15,8 @@ public class OptionsFunctions : MonoBehaviour
     public void Start()
     {
         //Sets the slider to be value of current Sensitivity to properly reflect previous settings/prevent from defaulting to different value
-        sensitivitySlider.value = CustomSens;
+        //sensitivitySlider.value = CustomSens;
+        sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity", DefaultCustomSens);
 
         //Adds listener at start of Frame to check for any value changes throughout.
         sensitivitySlider.onValueChanged.AddListener(delegate { ChangeSensitivity(); });
@@ -24,13 +25,14 @@ public class OptionsFunctions : MonoBehaviour
     //Changes sensitivity to reflect current slider value, refreshing on movement per listener above
     public void ChangeSensitivity()
     {
-        CustomSens = sensitivitySlider.value;
+        //CustomSens = sensitivitySlider.value;
+        PlayerPrefs.SetFloat("sensitivity", sensitivitySlider.value);
     }
 
     //Resets the sensitivity value to 5 (Default during design), and moves the slider accordingly
     public void ResetSensitivity()
     {
-        CustomSens = 5f;
-        sensitivitySlider.value = CustomSens;
+        PlayerPrefs.SetFloat("sensitivity", DefaultCustomSens);
+        //sensitivitySlider.value = DefaultCustomSens;
     }
 }
